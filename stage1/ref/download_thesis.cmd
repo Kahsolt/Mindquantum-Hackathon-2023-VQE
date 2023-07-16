cdf632e9777abfb985815b575645d6f53b26d878
Kahsolt <kahsolt@qq.com>
2023-07-16 21:04:03 +0800

    add thesis & lib

--- stage1/ref/download_thesis.cmd
+++ stage1/ref/download_thesis.cmd
@@ -8,3 +8,12 @@ wget -nc https://arxiv.org/pdf/1805.08138.pdf
 
 REM Subspace-search variational quantum eigensolver for excited states
 wget -nc https://arxiv.org/pdf/1810.09434.pdf
+
+REM Efficient Quantum Circuits for Accurate State Preparation of Smooth, Differentiable Functions
+wget -nc https://arxiv.org/pdf/2005.04351.pdf
+
+REM Simulated Quantum Computation of Molecular Energies
+wget -nc https://arxiv.org/ftp/quant-ph/papers/0604/0604193.pdf
+
+REM A Quantum-Quantum Metropolis Algorithm
+wegt -nc https://arxiv.org/pdf/1011.1468.pdf
